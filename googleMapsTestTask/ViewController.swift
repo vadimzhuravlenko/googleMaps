cad02aa839e5b02593e70f96aa1e2047f325918d
fnyekes <fnyekes@mail.ru>
2022-08-04 13:02:09 +0300

    second

--- googleMapsTestTask/ViewController.swift
+++ googleMapsTestTask/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
     
     let apiKey = "YOUR_API_KEY"
     let url = "https://fish-pits.krokam.by/api/rest/points/"
-    var arrLat = [Int]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         GMSServices.provideAPIKey(apiKey)
@@ -23,23 +21,17 @@ class ViewController: UIViewController {
         let camera = GMSCameraPosition.camera(withLatitude: 53.893009, longitude: 27.567444, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
         view.addSubview(mapView)
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2D(latitude: 53.893009, longitude: 27.567444)
-        marker.title = "Minsk"
-        marker.snippet = "Belarus"
-        marker.map = mapView
+        
         
         AF.request(url).responseJSON { response in
             switch response.result {
-            case .success(let data):
-//                print(data)
-                let value = JSON(data)
-                for index in 0...value.count-1 {
-                    print("\(value[index]["point"])")
-                    print(index)
-                    }
-                break
+            case .success:
+                let empty = try? JSONDecoder().decode(Empty.self, from: response.data!)
+                empty?.forEach({ element in
+                    let marker = GMSMarker()
+                    marker.position = CLLocationCoordinate2D(latitude: ((element.point?.lat)! )!, longitude: (element.point?.lng)!)
+                    marker.map = mapView
+                })
                 
             case .failure(let error):
                 print(error)
